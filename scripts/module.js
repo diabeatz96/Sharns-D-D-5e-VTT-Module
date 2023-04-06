68f744163bf9720812b6ef9ac90453dcfc680a04
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-04-06 14:57:17 -0400

    Get Actor Data, Set Actor Data, Get Actors in Array, SetActors etc.

--- scripts/module.js
+++ scripts/module.js
@@ -1,11 +1,69 @@
 
+/*
+        Helper functions to get Actor Data for pages and Edit those Actors
+*/
 
-console.log("Hello World from module.js");
+function getActorData(actorId) {
+    return game.actors.get(actorId).data;
+}
+
+function getAllActors() {
+    return game.actors;
+}
+
+function getAllActorIdsInArray() {
+    return game.actors.map(actor => actor.id);
+}
+
+function setActorData(actorId, data) {
+    game.actors.get(actorId).update(data);
+}
+
+class DeclasseData {
+
+}
 
 Hooks.once('init', async function() {
-
+   
+   
 });
 
 Hooks.once('ready', async function() {
+    const Actors = getAllActorIdsInArray();
+    console.log(Actors);
+});
+
+
+class Declasse {
+    static ID = 'Sharns-Declasse'
+
+
+    static FLAGS = {
+        DECLASSE: 'declasse'
+    }
+
+}
+
+
+
+
+/* Hooks */
+
+Hooks.on('updateActor', async function(actor, data, options, userId) {
+
+    console.log("Actor's health is " + actor.data.data.attributes.hp.value + userId);
+
+});
+
+
+Hooks.on('renderActorSheet', async function(sheet, html, data) {
+
+    getActorData(sheet.actor.id);
+
+    const table = html.find(".traits")
+
+    table.append(`"Heloooooooooooooooooooooooooooooooo"`);
+
+    sheet.actor.name = "Bob";
 
 });
