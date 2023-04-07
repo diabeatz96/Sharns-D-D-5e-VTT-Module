8998887832734d0bea548264cc31d4802b72b942
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-04-07 13:39:36 -0400

    Added on Character Render to show case FP AP and Mod of character

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,5 @@
 import { getActorData, getAllActors, getAllActorIdsInArray, setActorData } from "./helpers.js";
 
-
 class Declasse {
     static ID = "Sharns-Declasse";
     static FLAGS = {
@@ -16,9 +15,99 @@ class DeclasseData {
             ap: 12,
             mental: 0,
             physical: 0,
+            isCha: true,
+            isStr: true,
+            isDex: false,
+            isInt: false,
+            isWis: false,
+            isCon: false,
         }
         return getActorData(userId)?.setFlag(Declasse.ID, Declasse.FLAGS.atrributes, attributes);
     }
+
+    static getAttributes(userId) {
+        return getActorData(userId)?.getFlag(Declasse.ID, Declasse.FLAGS.atrributes);
+    }
+
+    static setAttributes(userId, attributes) {
+        return getActorData(userId)?.setFlag(Declasse.ID, Declasse.FLAGS.atrributes, attributes);
+    }
+
+    static decreaseFP(userId, amount) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.fp -= amount;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+    static decreaseAP(userId, amount) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.ap -= amount;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+    static increaseFP(userId, amount) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.fp += amount;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+    static increaseAP(userId, amount) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.ap += amount;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+    static setMentalModifiers(userId, attribute) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.isCha = false;
+        attributes.isInt = false;
+        attributes.isWis = false;
+        attributes[attribute] = true;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+    static setPhysicalModifiers(userId, attribute) {
+        const attributes = DeclasseData.getAttributes(userId);
+        attributes.isStr = false;
+        attributes.isDex = false;
+        attributes.isCon = false;
+        attributes[attribute] = true;
+        DeclasseData.setAttributes(userId, attributes);
+    }
+
+
+    static setMental(userId) {
+        
+        let total = 0;
+        if(DeclasseData.getAttributes(userId).isCha) {
+            total = getActorData(userId).system.abilities.cha.mod;
+            console.log(total);
+        }
+        if(DeclasseData.getAttributes(userId).isInt) {
+            total = getActorData(userId).system.abilities.int.mod;
+        }
+        if(DeclasseData.getAttributes(userId).isWis) {
+            total = getActorData(userId).system.abilities.wis.mod;
+        }
+        
+        DeclasseData.setAttributes(userId, {mental: total});
+    
+    }
+
+    static setPhysical(userId) {
+            let total = 0;
+    
+            if(DeclasseData.getAttributes(userId).isStr) {
+                total = getActorData(userId).system.abilities.str.mod;
+            }
+            if(DeclasseData.getAttributes(userId).isDex) {
+                total = getActorData(userId).system.abilities.dex.mod;
+            }
+            if(DeclasseData.getAttributes(userId).isCon) {
+                total = getActorData(userId).system.abilities.con.mod;
+            }
+            DeclasseData.setAttributes(userId, {physical: total});
+        }
 }
 
 /* Hooks */
@@ -29,42 +118,40 @@ Hooks.once('init', async function() {
    
 });
 
-Hooks.once('ready', async function() {
-
-
-    const Actors = getAllActorIdsInArray();
-    console.log(Actors);
-
-
-    for(let i = 0; i < Actors.length; i++) {
-        DeclasseData.createData(Actors[i]);
-        console.log(getActorData(Actors[i]))
-    }
-    //set flags for each actor with declasse
+Hooks.on('renderActorSheet5eCharacter', async function(sheet, html, data) {
     
-    console.log(getActorData(Actors[0]).getFlag(Declasse.ID, Declasse.FLAGS.atrributes));
+
+    if(!getActorData(sheet.actor.id).getFlag(Declasse.ID, Declasse.FLAGS.atrributes)) {
+        DeclasseData.createData(sheet.actor.id);
+        console.log("Created Data");
+    }
+
+    getActorData(sheet.actor.id);
+    const table = html.find(".traits")
+    table.prepend(`
+    <div class = "counter flexrow"> <h4 class="fas fa-hand-fist"> Physical Mod: </h4> <i></i> ${DeclasseData.getAttributes(sheet.actor.id).physical} </div>
+    <div class = "counter flexrow"> <h4 class = "fas fa-head-side-brain"> Mental Mod:</h4> <i></i> ${DeclasseData.getAttributes(sheet.actor.id).mental}</div>
+    <div class = "counter flexrow"> <h4 class = "fas fa-dragon">  FP: </h4> <i></i> ${DeclasseData.getAttributes(sheet.actor.id).fp}</div>
+    <div class = "counter flexrow"> <h4 class = "fas fa-hat-wizard"> AP: </h4> <i></i> ${DeclasseData.getAttributes(sheet.actor.id).ap} </div>`);
+
 });
 
+
+Hooks.once('ready', async function() {
+    getAllActorIdsInArray().forEach(actorId => {
+        if(!getActorData(actorId).getFlag(Declasse.ID, Declasse.FLAGS.atrributes)) {
+            DeclasseData.createData(actorId);
+            DeclasseData.setMental(actorId);
+            DeclasseData.setPhysical(actorId);
+            console.log("Created Data");
+        }
+    }); 
+});
 
 
 
 
 Hooks.on('updateActor', async function(actor, data, options, userId) {
-
-    console.log("Actor's health is " + actor.data.data.attributes.hp.value + userId);
-
-});
-
-
-Hooks.on('renderActorSheet', async function(sheet, html, data) {
-
-    getActorData(sheet.actor.id);
-
-    const table = html.find(".traits")
-
-    table.append(`"Heloooooooooooooooooooooooooooooooo"`);
-
-    sheet.actor.name = "Bob";
 
 });
 
